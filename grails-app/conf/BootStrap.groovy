91380a3d807abb19445a4710fb7f145b768dc6ea
Ken Kousen <ken.kousen@kousenit.com>
2008-09-10 03:31:35 -0400

    added jsecurity and ran quickstart

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
 class BootStrap {
 
      def init = { servletContext ->
+        if (!JsecRole.findByName('Administrator')) {
+          // Administrator user and role.
+          def adminRole = new JsecRole(name: "Administrator").save()
+          def adminUser = new JsecUser(username: "admin",
+              passwordHash: new Sha1Hash("p@ssw0rd").toHex()).save()
+          new JsecUserRoleRel(user: adminUser, role: adminRole).save()
+        }
      }
      def destroy = {
      }
